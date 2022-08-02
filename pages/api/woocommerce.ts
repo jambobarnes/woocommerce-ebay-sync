2be9fd205afdffde56846e6cc6ea9b303e88f361
James Barnes <james.barnes@approach-ps.com>
2022-08-02 19:28:15 +0100

    controller method filtering

--- pages/api/woocommerce.ts
+++ pages/api/woocommerce.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { checkSignature } from '../../lib/authCheck'
-import { Set202, Set401 } from '../../lib/responseHelpers';
+import { Set202, Set401, Set405 } from '../../lib/responseHelpers';
 import getRawBody from "raw-body"
 
 type Data = {
@@ -21,27 +21,33 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
 
-  // check if WC is sending a webhook id (happens after webhook is created or updated)
-  if (req.headers['content-type'] === "application/x-www-form-urlencoded") {
-    console.log('WC webhook checked in')
-    return Set202(res)
+  if (req.method === "POST") {
+
+    // check if WC is sending a webhook id (happens after webhook is created or updated)
+    if (req.headers['content-type'] === "application/x-www-form-urlencoded") {
+      console.log('WC webhook checked in')
+      return Set202(res)
+    }
+
+    // Payload and signature data for hashing and signature check
+    const rawBody = await getRawBody(req);
+    const signature: string = req?.headers?.['x-wc-webhook-signature']?.toString() || ''
+    const sigCheck = checkSignature(rawBody, signature)
+
+    if (!sigCheck) {
+      return Set401(res)
+    }
+
+    // parse raw body to json
+    const jsonBody = JSON.parse(rawBody.toString());
+
+    // send json body to be processed by WC service
+    const event: string = req?.headers?.['x-wc-webhook-topic']?.toString() || ''
+
+    return Set202(res);
+
   }
 
-  // Payload and signature data for hashing and signature check
-  const rawBody = await getRawBody(req);
-  const signature: string = req?.headers?.['x-wc-webhook-signature']?.toString() || ''
-  const sigCheck = checkSignature(rawBody, signature)
-
-  if (!sigCheck) {
-    return Set401(res)
-  }
-
-  // parse raw body to json
-  const jsonBody = JSON.parse(rawBody.toString());
-
-  // send json body to be processed by WC service
-  const event: string = req?.headers?.['x-wc-webhook-topic']?.toString() || ''
-
-  return Set202(res);
+  return Set405(res);
 
 }
