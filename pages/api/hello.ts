ac841dc17178f14bd7613538c79a276916563287
James Barnes <james.barnes@approach-ps.com>
2022-08-02 17:47:13 +0100

    added logging for test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -30,12 +30,16 @@ export default function handler(
   // return res.status(200).json({ name: 'John Doe' });
 
   const rawBody = req.body
+  console.log('RAWBODY', rawBody)
   const secret = 'james'
   const signature = req?.headers?.['x-wc-webhook-signature']
+  console.log('SIGNATURE', signature)
 
   const hash = createHmac('sha256', secret).update(rawBody).digest("base64");
+  console.log('HASH', hash)
 
   const match = hash === signature ? true : false
+  console.log('MATCH', match)
 
   return res.status(200).json({
     hash,
